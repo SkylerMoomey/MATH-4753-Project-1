760df491201056ca1c725331e51cce116bf66999
Skyler Moomey <skyler_moomey-1@ou.edu>
2020-09-28 18:31:02 -0500

    Completed project to best knowledge.

--- project1.R
+++ project1.R
@@ -99,4 +99,35 @@ ivg.tab <- with(swdefects.tab, table(predict.ivg.9.2, defect))
 
 twowaysum(ivg.tab, nrow(swdefects.tab))
 
+#Lines of Code through functions
 
+#function to calculate accuracy, detection rate,
+#false alarm rate, precision
+prob_measure=function(summary.tab)
+{
+  a=summary.tab[1,1]
+  b=summary.tab[1,2]
+  c=summary.tab[2,1]
+  d=summary.tab[2,2]
+
+  accuracy=acc(a, b, c, d)
+  detection=detect(b, d)
+  false_alarm=falarm(a, c)
+  precision=prec(c, d)
+
+  as.list(c(accuracy, detection, false_alarm, precision))
+}
+
+loc_measures <- prob_measure(loc.tab)
+cyclo_measures <-prob_measure(cyclo.tab)
+ivg_measures <- prob_measure(ivg.tab)
+evg_measures <- prob_measure(evg.tab)
+
+#table to store all values
+measures <- matrix(c("Method", "Accuracy", "Detection Rate"
+                     ,"False Alarm Rate", "Precision"
+  , "Lines of Code",loc_measures, "Cyclomatic Complexity", cyclo_measures
+  , "Essential Complexity", ivg_measures
+  , "Design Complexity", evg_measures)
+                   ,nrow=5, ncol=5, byrow=TRUE)
+measures
